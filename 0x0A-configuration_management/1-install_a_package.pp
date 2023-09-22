21e00e9f0a4adb2dd2b93b9999f1bd87601e42b9
globalsmile <mohammedbadmus823@gmail.com>
2023-09-22 16:39:33 -0700

    update: 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,26 @@
 # Install a package using Puppet
 
+exec { 'install python3-pip':
+  command => 'usr/bin/apt-get install python3-pip'
+}
+
+exec { 'install python3-venv':
+  command => 'usr/bin/apt-get install python3-venv'
+}
+
 package { 'python3-pip':
   ensure => installed,
+  before => Exec['install python3-pip']
 }
 
 package { 'python3-venv':
   ensure => installed,
+  before => Exec['install python3-venv'],
+  require => Packagae['python3-pip']
 }
 
 exec { 'install flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/local/bin:/usr/bin',
-  require => [Package['python3-venv'], Package['python3-pip']
+  require => [Package['python3-venv'], Package['python3-pip']]
 }
