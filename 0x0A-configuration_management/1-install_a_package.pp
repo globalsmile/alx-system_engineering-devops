31f4dc7a5173c54d75b6224465b4111cb860b737
globalsmile <mohammedbadmus823@gmail.com>
2023-09-22 16:41:36 -0700

    updated: 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,26 +1,20 @@
 # Install a package using Puppet
 
-exec { 'install python3-pip':
-  command => 'usr/bin/apt-get install python3-pip'
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
 }
 
-exec { 'install python3-venv':
-  command => 'usr/bin/apt-get install python3-venv'
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
 }
 
-package { 'python3-pip':
-  ensure => installed,
-  before => Exec['install python3-pip']
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
 }
 
-package { 'python3-venv':
-  ensure => installed,
-  before => Exec['install python3-venv'],
-  require => Packagae['python3-pip']
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  require => [Package['python3-venv'], Package['python3-pip']]
+package { 'puppet-lint':
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
 }
